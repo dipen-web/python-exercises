662f988a61b1c76bd817b86921f1bb774347e006
dipen-web <dipen.bakraniya1@gmail.com>
2025-08-18 14:21:28 +0530

    Add files via upload

---
+++ Day 4 - Count words and characters.py
@@ -0,0 +1,10 @@
+text = input("Enter string: ")
+
+# Write a program to count words and characters
+
+# words
+l1 = len(text.split())
+print(f"Number of Words - {l1}")
+
+# characters
+print(f"Number of Characters - {len(text)}")
